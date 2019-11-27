1cf2988c5be89b7186b9466a522f124252919d3d
CrisOspina <crisospina523@gmail.com>
2019-11-27 15:39:53 -0500

    [add] convert-routes-res-apis

--- projectbackend/src/routes/index.js
+++ projectbackend/src/routes/index.js
@@ -7,41 +7,62 @@ const Task = require('../models/task');
 //cuando pidan una peticion get
 
 //res.send('Hello world'); =>enviar mensaje
-router.get('/', async(req,res) =>{
-    //Pedir que muestre los datos agregados
-    const tasks = await Task.find();
-    console.log(tasks);
-    
-    //renderizar vista
-    res.render('index',{
-        tasks
-    });
+router.get('/', async (req, res) => {
+  //Pedir que muestre los datos agregados
+  const tasks = await Task.find();
+  console.log(tasks);
+  res.json({
+    data: tasks
+  });
+  //   renderizar vista
+  //   res.render('index', {
+  //     tasks
+  //   });
 });
-//Envio del form add
-router.post('/add', async (req,res) =>{
-    const task = new Task(req.body);
-    await task.save();
-    res.redirect('/');
+router.get('/libros', async (req, res) => {
+  //Pedir que muestre los datos agregados
+  const tasks = await Task.find();
+  console.log(tasks);
+
+  res.json({
+    data: tasks
+  });
+
+  //renderizar vista
+  //   res.render('index', {
+  //     tasks
+  //   });
 });
-//para actualizar datos
-router.get('/edit/:id', async (req,res) =>{
-    const { id } = req.params;
-    const task = await Task.findById(id);
-    res.render('edit',{
-        task
-    });
+//Envio del form add"prueba",
+router.post('/add', async (req, res) => {
+  const task = new Task(req.body);
+  await task.save();
+
+  res.json({
+    message: 'creado exitosamente',
+    data: task
+  });
+  //   res.redirect('/');
 });
+
 //para actualizar datos por POST
-router.post('/edit/:id', async (req,res) =>{
-    const { id } = req.params;
-    await Task.update({_id:id}, req.body);
-    res.redirect('/');
+router.put('/edit/:id', async (req, res) => {
+  const { id } = req.params;
+  await Task.update({ _id: id }, req.body);
+  //   res.redirect('/');
+  res.json({
+    message: 'Actualizado correctamente'
+  });
 });
 //para eliminar datos
-router.get('/delete/:id', async(req,res) =>{
-    const { id } = req.params;
-    await Task.remove({_id : id});
-    res.redirect('/');
+router.delete('/delete/:id', async (req, res) => {
+  const { id } = req.params;
+  await Task.remove({ _id: id });
+
+  //   res.redirect('/');
+  res.json({
+    message: 'Eliminado correctamente'
+  });
 });
 
 module.exports = router;
